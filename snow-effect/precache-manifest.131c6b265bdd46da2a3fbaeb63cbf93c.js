8cae37212e60fbb9a6c0eeffdab3d7b3848d57aa
Jin-Hyeok, Cha <Jin-Hyeok.Cha@outlook.kr>
2019-12-03 22:53:02 +0900

    add snow-effect bundle

---
+++ snow-effect/precache-manifest.131c6b265bdd46da2a3fbaeb63cbf93c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea3d54bec2d854fe05f96d00ec6c4c2d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c82353ab386c234e2a86",
+    "url": "./static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "985b52224b52ff76d71c",
+    "url": "./static/css/main.7d1e98da.chunk.css"
+  },
+  {
+    "revision": "c82353ab386c234e2a86",
+    "url": "./static/js/2.9f04b206.chunk.js"
+  },
+  {
+    "revision": "985b52224b52ff76d71c",
+    "url": "./static/js/main.557fd90f.chunk.js"
+  },
+  {
+    "revision": "26b9ad0ecbdf50be5cb7",
+    "url": "./static/js/runtime-main.7baeda72.js"
+  }
+]);
