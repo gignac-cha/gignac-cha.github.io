837e8c3d6fa592fbe8f9d0a948dbc3dda9e97a5c
Jin-Hyeok, Cha <Jin-Hyeok.Cha@outlook.kr>
2019-12-05 05:21:29 +0900

    edit index

--- index-project/src/App.js
+++ index-project/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row mt-4">
+        <div className="col col-12">
+          <h1>Index</h1>
+        </div>
+      </div>
+      <div className="row mt-4">
+        <div className="col col-12">
+          <h2>Projects</h2>
+          <ul>
+            <li><a href="sort/">Sorting Algorithms Simulator</a> <a href="https://github.com/gignac-cha/gignac-cha.github.io/tree/master/sort-project" target="_blank">(github)</a></li>
+            <li><a href="snow-effect/">Snowing Effect Simulator</a> <a href="https://github.com/gignac-cha/gignac-cha.github.io/tree/master/snow-effect-project" target="_blank">(github)</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
