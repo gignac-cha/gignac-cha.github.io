d41bd57d1bd7efb8012577e01e0faea384e1c41e
Jin-Hyeok, Cha <Jin-Hyeok.Cha@outlook.kr>
2022-08-10 18:53:33 +0900

    refactor

--- dot-blur/script.js
+++ dot-blur/script.js
@@ -1,59 +1,73 @@
-/** Original source from:
-  * @url https://heftyniceglitches--five-nine.repl.co/
-  * @date 2020-10-20
-*/
-
-const g = {
-  point: { x: 0, y: 0 },
-};
-const windowResized = e => {
-  const canvas = document.getElementById('canvas');
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+const resize = () => {
+  const canvas = document.querySelector('#canvas');
+  canvas.setAttribute('width', window.innerWidth);
+  canvas.setAttribute('height', window.innerHeight);
 };
 window.addEventListener('load', e => {
-  windowResized();
-  const canvas = document.getElementById('canvas');
+  resize();
+
+  const canvas = document.querySelector('#canvas');
+  const { width, height } = canvas;
   const context = canvas.getContext('2d');
-  setTimeout(update = () => {
-    const { width, height } = canvas;
-    const { point } = g;
+
+  const circle = (context, x, y, r, color) => {
+    context.beginPath();
+    context.fillStyle = color;
+    context.arc(x, y, r, 0, 2 * Math.PI);
+    context.fill();
+    context.closePath();
+  };
+  const rectangle = (context, x, y, width, height, color) => {
+    context.beginPath();
+    context.fillStyle = color;
+    context.rect(x, y, width, height);
+    context.fill();
+    context.closePath();
+  };
+
+  const radius = 10;
+  const size = radius * 6;
+  const w = Math.floor(width / size + 1);
+  const h = Math.floor(height / size + 1);
+
+  const mouse = {};
+
+  canvas.addEventListener('mousemove', e => {
+    const x = e.clientX;
+    const y = e.clientY;
+    mouse.move = { x, y };
+  });
+
+  requestAnimationFrame(update = () => {
+    requestAnimationFrame(update);
 
     context.clearRect(0, 0, width, height);
 
-    const size = parseInt(width / 20);
-    for (let j = parseInt(-height / size); j < height / size; ++j) {
-      for (let i = parseInt(-width / size); i < width / size; ++i) {
-        const x = size * i + width / 2;
-        const y = size * j + height / 2;
-        const dx = (x - point.x) / width;
-        const dy = (y - point.y) / height;
-        const r = size / 4 * (Math.sqrt(.25 + Math.abs(dx) * .75) + Math.sqrt(.25 + Math.abs(dy) * .75));
-        const cx = x + (size ** 2) * dx * (dy ** 2);
-        const cy = y + (size ** 2) * dy * (dx ** 2);
-        const d = Math.abs(dx) + Math.abs(dy);
-        const blur = r + 1 + size / 2 * d;
-        const rg = context.createRadialGradient(...[cx, cy, r, cx, cy, blur].map(x => parseInt(x)));
-        const c = 256 - d * 128;
-        rg.addColorStop(0, `rgba(${c}, ${c}, ${c}, 1)`);
-        rg.addColorStop(.5, `rgba(${c}, ${c}, ${c}, .5)`);
-        rg.addColorStop(1, `rgba(${c}, ${c}, ${c}, 0)`);
-        context.fillStyle = rg;
-        context.fillRect(cx - blur, cy - blur, blur * 2, blur * 2);
+    for (let j = 0; j < h; ++j) {
+      for (let i = 0; i < w; ++i) {
+        const x = size * i + radius;
+        const y = size * j + radius;
+        if (mouse.move) {
+          const dx = (x - mouse.move.x) / width;
+          const dy = (y - mouse.move.y) / height;
+          const r = radius * ((.25 + Math.abs(dx) * .75) ** .5 + (.25 + Math.abs(dy) * .75) ** .5);
+          const cx = x + (size ** 2) * dx * (dy ** 2);
+          const cy = y + (size ** 2) * dy * (dx ** 2);
+          const d = Math.abs(dx) + Math.abs(dy);
+          const blur = r + 1 + size * d;
+          const rg = context.createRadialGradient(cx, cy, r, cx, cy, blur);
+          const c = 0xff - d * 0x7f;
+          rg.addColorStop(0, `rgba(${c}, ${c}, ${c}, 1)`);
+          rg.addColorStop(.5, `rgba(${c}, ${c}, ${c}, .5)`);
+          rg.addColorStop(1, `rgba(${c}, ${c}, ${c}, 0)`);
+          rectangle(context, cx - blur, cy - blur, blur * 2, blur * 2, rg);
+        } else {
+          circle(context, x, y, radius, 'rgba(255, 255, 255, .1)');
+        }
       }
     }
-
-    setTimeout(update, 1000 / 60);
   });
 });
-window.addEventListener('resize', windowResized);
-window.addEventListener('mousemove', e => {
-  g.point.x = e.clientX;
-  g.point.y = e.clientY;
-});
-window.addEventListener('touchmove', e => {
-  Array.from(e.touches).forEach(t => {
-    g.point.x = t.clientX;
-    g.point.y = t.clientY;
-  });
+window.addEventListener('resize', e => {
+  resize();
 });
