fa9e8a53f1062ec772cce1db731553c98e660247
Jin-Hyeok, Cha <Jin-Hyeok.Cha@outlook.kr>
2022-08-11 02:52:32 +0900

    refactor

--- maze-pathfinder/script.js
+++ maze-pathfinder/script.js
@@ -1,187 +1,161 @@
-function random(...args) {
-  const r = Math.random();
-  if (args.length === 1) {
-    const [first] = args;
-    if (Number.isInteger(first)) {
-      return Math.floor(r * first);
-    } else if (Array.isArray(first)) {
-      const array = first;
-      return array[random(array.length)];
-    }
-  } else if (args.length === 2) {
-    const [start, end] = args;
-    if (Number.isInteger(start) && Number.isInteger(end)) {
-      return Math.floor(start + r * (end - start));
-    }
-  }
-  return r;
-}
-
-function range(value, start, end) {
-  return start <= value && value < end;
-}
-
-function createMaze(width, height) {
-  const maze = Array.from(new Array(width)).map(() => Array.from(new Array(height)).map(() => 0));
-  for (let i = 0; i < 100; ++i) {
-    const x = random(width);
-    const y = random(height);
-    if (range(x, 0, 10) && range(y, 0, 10)) {
-      continue;
-    } else if (range(x, width - 10, width) && range(y, height - 10, height)) {
-      continue;
-    }
-    const a = random(2);
-    const d = random([-1, 1]);
-    const l = random(5, 10);
-    try {
-      if (a) {
-        for (let j = 0; j < l && range(x + d * j, 0, width); ++j) {
-          maze[y][x + d * j] = 9;
-        }
-      } else {
-        for (let j = 0; j < l && range(y + d * j, 0, height); ++j) {
-          maze[y + d * j][x] = 9;
-        }
-      }
-    } catch (e) {
-      console.error({ x, y, a, d, l });
-      throw e;
-    }
-  }
-  return maze;
-}
-
-const g = {
-  fps: 60,
-  play: true,
-  width: 50,
-  height: 50,
-  size: 10,
-  stack: [],
-  path: [],
-  queue: [],
-  directions: [
-    { dx: 1, dy: 0 },
-    { dx: 0, dy: 1 },
-    { dx: -1, dy: 0 },
-    { dx: 0, dy: -1 },
-  ],
-  current: { x: 0, y: 0 },
-};
-g.goal = { x: g.width - 1, y: g.height - 1 };
-g.lastDirection = g.directions[0];
+import Canvas from '../modules/canvas.js';
 
 window.addEventListener('load', e => {
-  const canvas = document.createElement('canvas');
-  document.body.appendChild(canvas);
-  canvas.width = 1 + g.size * g.width + 1;
-  canvas.height = 1 + g.size * g.height + 1;
+  const canvas = new Canvas(document.querySelector('#canvas'));
 
-  const button = document.createElement('button');
-  document.body.appendChild(button);
-  button.innerText = '||';
-  button.addEventListener('click', e => {
-    button.innerText = g.play ? '|>' : '||';
-    g.play = !g.play;
-  });
+  const elements = {
+    debug: document.querySelector('#debug'),
+  };
 
-  const maze = createMaze(g.width, g.height);
-  maze[g.goal.y][g.goal.x] = 2;
+  const size = 10;
+  const w = 50;
+  const h = 50;
+  const width = (1 + w + 1) * size;
+  const height = (1 + h + 1) * size;
+  canvas.width = width;
+  canvas.height = height;
 
-  setTimeout(update = () => {
-    const { width, height } = canvas;
+  const getRandomReal = n => Math.random() * n;
+  const getRandom = n => Math.floor(getRandomReal(n));
 
-    const context = canvas.getContext('2d');
-    context.clearRect(0, 0, width, height);
+  const STATE_EMPTY = 0;
+  const STATE_WALL = 1;
+  const STATE_CURRENT = 2;
+  const STATE_PATH = 3;
+  const STATE_VISITED = 4;
+  const STATE_START = 8;
+  const STATE_GOAL = 9;
 
-    context.beginPath();
-    context.strokeStyle = 'white';
-    context.moveTo(0, 0);
-    context.lineTo(width, 0);
-    context.lineTo(width, height);
-    context.lineTo(0, height);
-    context.lineTo(0, 0);
-    context.stroke();
-    context.closePath();
-
-    for (let y = 0; y < g.height; ++y) {
-      for (let x = 0; x < g.width; ++x) {
-        if (maze[y][x] === 0) {
-          context.fillStyle = 'gray';
-        } else if (maze[y][x] === 1) {
-          context.fillStyle = 'lightblue';
-        } else if (maze[y][x] === 8) {
-          context.fillStyle = 'lightpink';
-        } else if (maze[y][x] === 9) {
-          context.fillStyle = 'black';
+  const createMaze = () => {
+    const maze = new Array(w * h).fill(0);
+    for (let i = 0; i < 100; ++i) {
+      const x = getRandom(w);
+      const y = getRandom(h);
+      if (getRandom(100) < 50) {
+        for (let j = 0, count = 5 + getRandom(5); j < count; ++j) {
+          if (x + j < w) {
+            maze[y * w + x + j] = STATE_WALL;
+          }
         }
-        context.fillRect(1 + g.size * x, 1 + g.size * y, g.size, g.size);
-      }
-    }
-    context.fillStyle = 'green';
-    g.path.forEach(({ x, y }) =>
-      context.fillRect(1 + g.size * x + g.size / 4, 1 + g.size * y + g.size / 4, g.size / 2, g.size / 2)
-    );
-    context.fillStyle = 'lightgreen';
-    context.fillRect(1 + g.size * g.current.x, 1 + g.size * g.current.y, g.size, g.size);
-    context.fillStyle = 'red';
-    context.fillRect(1 + g.size * g.goal.x, 1 + g.size * g.goal.y, g.size, g.size);
-
-    setTimeout(update, 1000 / g.fps);
-  });
-
-  setTimeout(find = () => {
-    let goal = false;
-    let fail = false;
-    if (g.play) {
-      if (g.stack.length === 0) {
-        const { x, y } = g.current;
-        g.directions
-          .reverse()
-          .filter(({ dx }) => range(x + dx, 0, g.width))
-          .filter(({ dy }) => range(y + dy, 0, g.height))
-          .filter(({ dx, dy }) => maze[y + dy][x + dx] === 0)
-          .forEach(({ dx, dy }) => g.stack.push({ x: x + dx, y: y + dy }));
       } else {
-        const { current } = g;
-        const { x, y } = current;
-        const index = g.directions.indexOf(g.directions.find(({ dx, dy }) => dx === g.lastDirection.dx && dy === g.lastDirection.dy));
-        const rotatedDirections = [...g.directions.slice(index), ...g.directions.slice(0, index)];
-        const edgeFiltered = rotatedDirections
-          .reverse()
-          .filter(({ dx }) => range(x + dx, 0, g.width))
-          .filter(({ dy }) => range(y + dy, 0, g.height));
-        if (edgeFiltered.some(({ dx, dy }) => maze[y + dy][x + dx] === 2)) {
-          goal = true;
-          g.path.push(current);
-          maze[y][x] = 1;
-          g.current = g.goal;
-        } else {
-          const pathFiltered = edgeFiltered.filter(({ dx, dy }) => maze[y + dy][x + dx] === 0);
-          pathFiltered.forEach(({ dx, dy }) => g.stack.push({ x: x + dx, y: y + dy }));
-          if (pathFiltered.length > 0) {
-            g.path.push(current);
-            maze[y][x] = 1;
-            g.current = g.stack.pop();
-            g.lastDirection = { dx: g.current.x - x, dy: g.current.y - y };
-          } else {
-            maze[y][x] = 8;
-            if (g.path.length > 0) {
-              g.current = g.path.pop();
-            } else {
-              fail = true;
-            }
+        for (let j = 0, count = 5 + getRandom(5); j < count; ++j) {
+          if (y + j < h) {
+            maze[(y + j) * h + x] = STATE_WALL;
           }
         }
       }
     }
-    if (goal) {
-      console.log('path found');
-    } else if (fail) {
-      console.error('path not found');
-    } else {
-      setTimeout(find, 1000 / (g.fps / 1));
+    maze[0] = STATE_START;
+    maze[maze.length - 1] = STATE_GOAL;
+    return maze;
+  };
+
+  const maze = createMaze();
+  const position = { x: 0, y: 0 };
+  const directions = [
+    { dx: 1, dy: 0 },
+    { dx: 0, dy: 1 },
+    { dx: -1, dy: 0 },
+    { dx: 0, dy: -1 }
+  ];
+  const visited = [0];
+
+  const count = {
+    visited: 0,
+    path: 0,
+  };
+
+  requestAnimationFrame(function update() {
+    requestAnimationFrame(update);
+
+    canvas.clear();
+
+    canvas.rectangle(0, 0, width, size, 'black').fill();
+    canvas.rectangle(width - size, 0, size, height, 'black').fill();
+    canvas.rectangle(0, height - size, width, size, 'black').fill();
+    canvas.rectangle(0, 0, size, height, 'black').fill();
+
+    for (let i = 0; i < maze.length; ++i) {
+      const x = 1 + i % w;
+      const y = 1 + Math.floor(i / h);
+      switch (maze[i]) {
+        case STATE_EMPTY:
+          break;
+        case STATE_WALL:
+          canvas.rectangle(x * size, y * size, size, size, 'black').fill();
+          break;
+        case STATE_CURRENT:
+          canvas.rectangle(x * size, y * size, size, size, 'green').fill();
+          break;
+        case STATE_PATH:
+          canvas.rectangle(x * size, y * size, size, size, 'lightgreen').fill();
+          canvas.rectangle(x * size + size / 4, y * size + size / 4, size / 2, size / 2, 'green').fill();
+          break;
+        case STATE_VISITED:
+          canvas.rectangle(x * size, y * size, size, size, 'pink').fill();
+          break;
+        case STATE_START:
+          canvas.rectangle(x * size, y * size, size, size, 'blue').fill();
+          break;
+        case STATE_GOAL:
+          canvas.rectangle(x * size, y * size, size, size, 'red').fill();
+          break;
+      }
     }
+
+    const key1 = position.y * w + position.x;
+    if (maze[key1] === STATE_GOAL) {
+      elements.debug.textContent = `Path found! (Visited count: ${count.visited}, Path length: ${count.path})`;
+      return;
+    }
+
+    for (let i = 0; i < directions.length; ++i) {
+      const { dx, dy } = directions[0];
+      const x = position.x + dx;
+      const y = position.y + dy;
+      const key2 = y * w + x;
+      if (0 <= x && x < w && 0 <= y && y < h) {
+        if (maze[key2] === STATE_EMPTY) {
+          position.x = x;
+          position.y = y;
+          if (maze[key1] !== STATE_START) {
+            maze[key1] = STATE_PATH;
+            visited.push(key1);
+          }
+          maze[key2] = STATE_CURRENT;
+          count.visited++;
+          count.path++;
+          break;
+        } else if (maze[key2] === STATE_GOAL) {
+          position.x = x;
+          position.y = y;
+          maze[key1] = STATE_PATH;
+          elements.debug.textContent = 'Path found!';
+          count.visited++;
+          count.path++;
+          return;
+        }
+      }
+      directions.push(directions.shift());
+    }
+    const key2 = position.y * w + position.x;
+    if (key1 === key2) {
+      if (visited.length > 0) {
+        const key3 = visited.pop();
+        const x = key3 % w;
+        const y = Math.floor(key3 / h);
+        position.x = x;
+        position.y = y;
+        maze[key2] = STATE_VISITED;
+        maze[key3] = STATE_CURRENT;
+        count.visited++;
+        count.path--;
+      } else {
+        elements.debug.textContent = `Path not found! (Visited count: ${count.visited})`;
+        return;
+      }
+    }
+
+    elements.debug.textContent = `Finding path... (Visited count: ${count.visited}, Path length: ${count.path})`;
   });
 });
