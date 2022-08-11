3184115153bc53bc0893c095e1118c08d5d70b46
Jin-Hyeok, Cha <Jin-Hyeok.Cha@outlook.kr>
2022-08-11 09:51:03 +0900

    refactor

--- dot-adjacent-points/script.js
+++ dot-adjacent-points/script.js
@@ -1,136 +1,50 @@
-(function () { var script = document.createElement('script'); script.onload = function () { var stats = new Stats(); document.body.appendChild(stats.dom); requestAnimationFrame(function loop() { stats.update(); requestAnimationFrame(loop) }); }; script.src = '//mrdoob.github.io/stats.js/build/stats.min.js'; document.head.appendChild(script); })()
+import Canvas from '../modules/canvas.js';
+import { random } from '../modules/random.js';
+import { range } from '../modules/range.js';
 
-$.fn.extend({
-  shape: function (width, height) {
-    if (typeof width === 'number' && typeof height === 'number') {
-      return this.attr({ width, height });
-    } else {
-      const width = this.width();
-      const height = this.height();
-      return { width, height };
-    }
-  },
-});
-
-const g = {
-  points: [],
-  finished: -1,
-  connectionCount: 10,
+const resize = canvas => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 };
+window.addEventListener('load', e => {
+  const canvas = new Canvas(document.querySelector('#canvas'));
 
-function resize() {
-  const { width, height } = $(window).shape();
-  $('#canvas').shape(width, height);
-}
+  resize(canvas);
 
-function getColor(rgba) {
-  const { r, g, b, a } = rgba;
-  return `rgba(${r}, ${g}, ${b}, ${a})`;
-}
-function getRandomRGB() {
-  const r = Math.random() * 256;
-  const g = Math.random() * 256;
-  const b = Math.random() * 256;
-  return { r, g, b, a: 1 };
-}
-function getRandomColor() {
-  return getColor(getRandomRGB());
-}
+  const { width, height } = canvas;
 
-$(window).on('load', e => {
-  resize();
+  const points = [];
+  const distances = [];
 
-  const { width, height } = $(window).shape();
-  for (let i = 0; i < 1000; ++i) {
-    // const x = Math.random() * width;
-    // const y = Math.random() * height;
-    const d = Math.random() * 360;
-    const r = d / 360 * 2 * Math.PI;
-    const sin = Math.sin(r);
-    const cos = Math.cos(r);
-    const l = Math.tan(Math.random()) * (width ** 2 + height ** 2) ** .5 / 4;
-    const x = width / 2 + cos * l;
-    const y = height / 2 + sin * l;
-    const rgba = getRandomRGB();
-    g.points.push({ i, x, y, rgba });
-  }
+  const getDistance = (point1, point2) => ((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2) ** .5;
 
-  const canvas = document.getElementById('canvas');
-
-  requestAnimationFrame(update = () => {
+  requestAnimationFrame(function update() {
     requestAnimationFrame(update);
 
-    const { width, height } = $(window).shape();
-    const context = canvas.getContext('2d');
-    context.clearRect(0, 0, width, height);
+    canvas.clear();
 
-    context.fillStyle = 'black';
-    context.strokeStyle = 'white';
-    // for (let i = 0; i < g.points.length; ++i) {
-    //   const point = g.points[i];
-    //   context.beginPath();
-    //   context.arc(point.x, point.y, 2, 0, 2 * Math.PI);
-    //   context.fill();
-    //   context.stroke();
-    //   context.closePath();
-    //   if (!point.painted) {
-    //     point.painted = true;
-    //     break;
-    //   }
-    // }
-    for (let point of g.points) {
-      context.beginPath();
-      context.arc(point.x, point.y, 2, 0, 2 * Math.PI);
-      context.fill();
-      context.stroke();
-      context.closePath();
-      if (!point.painted) {
-        point.painted = true;
-        break;
+    for (let i = 0; i < points.length; ++i) {
+      canvas.circle(points[i].x, points[i].y, 2, 'white').fill();
+      const targets = distances[i].map((distance, i) => ({ i, distance }));
+      targets.sort((a, b) => a.distance - b.distance);
+      for (const target of targets.slice(1, 11)) {
+        canvas.line(points[i], points[target.i], 'rgba(255, 255, 255, .1)').stroke();
       }
     }
-    context.strokeStyle = 'rgba(255, 255, 255, .2)';
-    // g.points.forEach(point => point.connected = false);
-    const filtered = g.points.filter(point => point.painted);
-    filtered.forEach(point => {
-      const { i, x, y } = point;
-      // const notConnected = filtered.filter(p => p.i !== i && !p.connected);
-      // const sorted = Array.from(notConnected)
-      const sorted = Array.from(filtered.filter(p => p.i !== i))
-        .sort((p1, p2) => {
-          const d1 = (p1.x - x) ** 2 + (p1.y - y) ** 2;
-          const d2 = (p2.x - x) ** 2 + (p2.y - y) ** 2;
-          return d1 - d2;
-        });
-      const ps = sorted.slice(0, g.connectionCount);
-      ps.forEach(p => {
-        context.beginPath();
-        context.moveTo(x, y);
-        context.lineTo(p.x, p.y);
-        context.stroke();
-        context.closePath();
-      });
-      // point.connected = true;
-      // filtered.filter(p => p.i !== i).forEach(p => {
-      //   context.beginPath();
-      //   context.moveTo(x, y);
-      //   context.lineTo(p.x, p.y);
-      //   context.stroke();
-      //   context.closePath();
-      // });
-    });
+
+    const point = { x: random(width), y: random(height) };
+    distances.push([]);
+    for (let i = 0; i < points.length; ++i) {
+      const distance = getDistance(points[i], point);
+      distances[i].push(distance);
+    }
+    for (let i = 0; i < points.length; ++i) {
+      distances[distances.length - 1].push(distances[i][distances.length - 1]);
+    }
+    distances[distances.length - 1].push(0);
+    points.push(point);
   });
-}).on('resize', e => {
+});
+window.addEventListener('resize', e => {
   resize();
-  // }).on('mousemove', e => {
-  //   const x = e.clientX;
-  //   const y = e.clientY;
-  //   g.mousemove = { x, y };
-  //   updatePoints(x, y);
-  // }).on('touchmove', e => {
-  //   const touch = Array.from(e.touches).shift();
-  //   const x = touch.clientX;
-  //   const y = touch.clientY;
-  //   g.touchmove = { x, y };
-  //   updatePoints(x, y);
 });
